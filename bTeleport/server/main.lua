2d6d87ae4b42ed486aa2e4383b491ee506eeeb5a
Barwoz.Dev <85193395+Barwoz@users.noreply.github.com>
2022-06-29 12:59:59 +0200

    Update main.lua

--- bTeleport/server/main.lua
+++ bTeleport/server/main.lua
@@ -3,4 +3,4 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 print("^0| ^1TELEPORT^7 | ^0CREATED BY^7 ^2BARWOZ^7 | ^3FOR B SHOP^7")
-print("^0| ^1OUR SHOP^7 | ^4https://memorys-fivem.tebex.io/^7")
+print("^0| ^1OUR SHOP^7 | ^4https://memory-s.fr^7")
